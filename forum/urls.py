9405ff23b307c71bd2f667d18d55bc0987e4ba13
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2022-07-13 11:14:22 +0430

    Create Room, Topic and Message models

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'forum'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
+    path('room/<str:pk>', views.room, name='room'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from .models import Room, Message
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html')
+    room_list = Room.objects.all()
+    context = {'rooms': room_list}
+    return render(request, 'home.html', context)
 
 
-def room(request):
-    pass
+def room(request, pk):
+    room_detail = Room.objects.get(id=pk)
+    context = {'room': room_detail}
+    return render(request, 'room.html', context)
